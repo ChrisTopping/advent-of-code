536e9b3ccd0e42543a7d878465a1a79611aa819c
ctopping <chris.p.topping@gmail.com>
2021-12-10 05:25:00 +0000

    Added day 10 part 2 template

---
+++ src/main/java/com/cpt/adventofcode/solution/year2021/day10/Day10Part2Solution.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2021.day10;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2021, day = 10, part = 2,
+        description = "Syntax Scoring",
+        link = "https://adventofcode.com/2021/day/10",
+        tags = ""
+)
+public class Day10Part2Solution implements Solution<Long> {
+
+    @Override
+    public Long solve(Stream<String> inputStream) {
+        return 0L;
+    }
+
+}
