dcb1535416836fff6a8c3bc2529dda74ca45a236
ctopping <chris.p.topping@gmail.com>
2021-12-11 00:22:33 +0000

    Fixed failing tests

--- src/integrationTest/java/com/cpt/adventofcode/solution/year2021/day10/Day10Part2SolutionTest.java
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2021/day10/Day10Part2SolutionTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 class Day10Part2SolutionTest extends BaseTest<Long> {
 
     @Test
-    @DisplayName("Given test input: should return 0L")
-    void givenTestInput_ShouldReturn0L() {
+    @DisplayName("Given test input: should return 2904180541L")
+    void givenTestInput_ShouldReturn2904180541L() {
         Day10Part2Solution solution = new Day10Part2Solution();
-        assertSolutionIsCorrect(solution, 0L);
+        assertSolutionIsCorrect(solution, 2904180541L);
     }
 
 }
