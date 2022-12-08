84ef6928dad0e62f40dee7612fbe1671ff89c538
ctopping <chris.p.topping@gmail.com>
2022-12-08 01:08:27 +0000

    Added 2022 day 6 solutions

---
+++ src/main/java/com/cpt/adventofcode/solution/year2022/day6/Day6Part2Solution.java
@@ -0,0 +1,21 @@
+package com.cpt.adventofcode.solution.year2022.day6;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2022, day = 6, part = 2,
+        description = "Tuning Trouble",
+        link = "https://adventofcode.com/2022/day/6#part2",
+        tags = ""
+)
+public class Day6Part2Solution implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        String input = inputStream.findFirst().orElse("");
+        return Subroutine.findMarker(14, input);
+    }
+
+}
