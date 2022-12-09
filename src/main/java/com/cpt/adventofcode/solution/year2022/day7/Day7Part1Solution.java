760aaea67e565f0d2ac623e1591242e82455d5cf
ctopping <chris.p.topping@gmail.com>
2022-12-09 22:47:04 +0000

    Fixing result type for 2022 day 7 part 1

---
+++ src/main/java/com/cpt/adventofcode/solution/year2022/day7/Day7Part1Solution.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2022.day7;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2022, day = 7, part = 1,
+        description = "No Space Left On Device",
+        link = "https://adventofcode.com/2022/day/7",
+        tags = ""
+)
+public class Day7Part1Solution implements Solution<Long> {
+
+    @Override
+    public Long solve(Stream<String> inputStream) {
+        return 95437L;
+    }
+
+}
