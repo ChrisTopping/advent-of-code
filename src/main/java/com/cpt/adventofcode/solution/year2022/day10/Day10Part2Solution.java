674f5323f5b8b1962a0e9cdc683f35ce54522b51
ctopping <chris.p.topping@gmail.com>
2023-11-19 18:50:08 +0000

    Added 2022 day 10 part 2

---
+++ src/main/java/com/cpt/adventofcode/solution/year2022/day10/Day10Part2Solution.java
@@ -0,0 +1,21 @@
+package com.cpt.adventofcode.solution.year2022.day10;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2022, day = 10, part = 2,
+        description = "Cathode-Ray Tube",
+        link = "https://adventofcode.com/2022/day/10",
+        tags = ""
+)
+public class Day10Part2Solution implements Solution<String> {
+
+    @Override
+    public String solve(Stream<String> inputStream) {
+        Register register = Register.initialise(inputStream);
+        return "\n" + register.getPixels();
+    }
+
+}
