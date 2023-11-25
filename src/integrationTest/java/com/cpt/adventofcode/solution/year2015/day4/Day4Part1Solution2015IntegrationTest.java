33ab62d3e796bc49ad9276c40baf73b81ff6261d
ctopping <chris.p.topping@gmail.com>
2023-11-25 17:21:22 +0000

    Added 2015 day 4 parts 1 and 2.

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2015/day4/Day4Part1Solution2015IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2015.day4;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 4 Part 1")
+class Day4Part1Solution2015Test extends BaseTest<Long> {
+
+    @Test
+    @DisplayName("Given test input: should return 1048970")
+    void givenTestInput_ShouldReturn1048970() {
+        Day4Part1Solution2015 solution = new Day4Part1Solution2015();
+        assertSolutionIsCorrect(solution, 117946L);
+    }
+
+}
---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2015/day4/Day4Part2Solution2015IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2015.day4;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 4 Part 2")
+class Day4Part2Solution2015Test extends BaseTest<Long> {
+
+    @Test
+    @DisplayName("Given test input: should return ")
+    void givenTestInput_ShouldReturn() {
+        Day4Part2Solution2015 solution = new Day4Part2Solution2015();
+        assertSolutionIsCorrect(solution, 3938038L);
+    }
+
+}
