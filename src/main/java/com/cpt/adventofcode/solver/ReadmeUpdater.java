e0174141fdbb54e054fdcd8b9dfee0eddd6f8c58
ctopping <chris.p.topping@gmail.com>
2023-12-02 16:58:34 +0000

    Update README based on readme solver flag.

--- src/main/java/com/cpt/adventofcode/solver/ReadmeUpdater.java
+++ src/main/java/com/cpt/adventofcode/solver/ReadmeUpdater.java
@@ -4,6 +4,7 @@ import com.google.common.io.Files;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,17 +15,23 @@ public class ReadmeUpdater {
     public static void replaceReadmeResults(String results) throws IOException {
         String readmeContent = getReadmeContent();
 
-        Pattern pattern = Pattern.compile("```results.*```", Pattern.DOTALL);
-        readmeContent = pattern.matcher(readmeContent).replaceFirst("```results\n" + results + "```");
+        Pattern pattern = Pattern.compile("```results.*---", Pattern.DOTALL);
+        readmeContent = pattern.matcher(readmeContent).replaceFirst("```results\n" + results + "```\n---")
+                .replaceAll("\u001B\\[\\d+;\\d+;\\d+;\\d+;0m", "")
+                .replaceAll("\u001B\\[0m", "");
 
-
-        System.out.println(readmeContent);
+        updateReadme(readmeContent);
     }
 
 
     private static String getReadmeContent() throws IOException {
         Path path = getRelativePath("README.md");
         return String.join("\n", Files.readLines(path.toFile(), Charset.defaultCharset()));
+    }
+
+    private static void updateReadme(String newContentString) throws IOException {
+        Path path = getRelativePath("README.md");
+        Files.write(newContentString.getBytes(StandardCharsets.UTF_8), path.toFile());
     }
 
     private static Path getRelativePath(String... subpaths) {
