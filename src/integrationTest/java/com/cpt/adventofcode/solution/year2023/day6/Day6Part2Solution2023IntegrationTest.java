9b0b3e0707cb23a237c445ed627d08c7e7c75848
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-06 05:01:47 +0000

    Adding today's problem

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part2Solution2023IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day6;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 6 Part 2")
+class Day6Part2Solution2023IntegrationTest extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day6Part2Solution2023 solution = new Day6Part2Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
---
+++ src/test/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part1Solution2023Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day6;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 6 Part 1")
+class Day6Part1Solution2023Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day6Part1Solution2023 solution = new Day6Part1Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
