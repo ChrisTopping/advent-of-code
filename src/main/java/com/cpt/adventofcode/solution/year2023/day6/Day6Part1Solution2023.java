c44db343f768db6b74bf9f7ba0a2fdc550853ed3
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-06 05:01:47 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part1Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day6;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 6, part = 1,
+        description = "Wait For It",
+        link = "https://adventofcode.com/2023/day/6",
+        tags = ""
+)
+public class Day6Part1Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
