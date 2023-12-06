9cbd9491929cbc5efee9519addb8c43fa5ccabc4
ctopping <chris.p.topping@gmail.com>
2023-12-06 12:27:05 +0000

    Added solutions for 2023 day 6 parts 1 and 2. Added !unsolved filter on runner.

--- src/main/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part1Solution2023.java
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part1Solution2023.java
@@ -1,8 +1,11 @@
 package com.cpt.adventofcode.solution.year2023.day6;
 
 import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.helpers.Strings;
 import com.cpt.adventofcode.solution.Solution;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @AdventOfCodeSolution(year = 2023, day = 6, part = 1,
@@ -14,7 +17,25 @@ public class Day6Part1Solution2023 implements Solution<Integer> {
 
     @Override
     public Integer solve(Stream<String> inputStream) {
-        return 0;
+        List<String> input = Strings.toList(inputStream);
+
+        String timesString = input.get(0).replace("Time:", "").trim();
+        List<Integer> times = Strings.splitAndCast(timesString, "\\s+", Strings.Type.INTEGER);
+
+        String distancesString = input.get(1).replace("Distance:", "").trim();
+        List<Integer> distances = Strings.splitAndCast(distancesString, "\\s+", Strings.Type.INTEGER);
+
+        return IntStream.range(0, times.size())
+                .map(race -> countBetterRaceDistances(times.get(race), distances.get(race)))
+                .reduce((left, right) -> left * right)
+                .orElseThrow();
+    }
+
+    private int countBetterRaceDistances(long time, long distance) {
+        double discriminant = Math.sqrt(Math.pow(time, 2) - 4 * distance);
+        int positiveRoot = (int) Math.ceil((time + discriminant) / 2);
+        int negativeRoot = (int) Math.floor((time - discriminant) / 2);
+        return positiveRoot - negativeRoot - 1;
     }
 
 }
--- src/main/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part2Solution2023.java
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day6/Day6Part2Solution2023.java
@@ -1,8 +1,10 @@
 package com.cpt.adventofcode.solution.year2023.day6;
 
 import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.helpers.Strings;
 import com.cpt.adventofcode.solution.Solution;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @AdventOfCodeSolution(year = 2023, day = 6, part = 2,
@@ -10,11 +12,30 @@ import java.util.stream.Stream;
         link = "https://adventofcode.com/2023/day/6#part2",
         tags = ""
 )
-public class Day6Part2Solution2023 implements Solution<Integer> {
+public class Day6Part2Solution2023 implements Solution<Long> {
 
     @Override
-    public Integer solve(Stream<String> inputStream) {
-        return 0;
+    public Long solve(Stream<String> inputStream) {
+        List<String> input = Strings.toList(inputStream);
+
+        String timeString = input.get(0)
+                .replace("Time:", "")
+                .replaceAll("\\s+", "");
+        long time = Long.parseLong(timeString);
+
+        String distanceString = input.get(1)
+                .replace("Distance:", "")
+                .replaceAll("\\s+", "");
+        long distance = Long.parseLong(distanceString);
+
+        return countBetterRaceDistances(time, distance);
+    }
+
+    private long countBetterRaceDistances(long time, long distance) {
+        double discriminant = Math.sqrt(Math.pow(time, 2) - 4 * distance);
+        long positiveRoot = (long) Math.ceil((time + discriminant) / 2);
+        long negativeRoot = (long) Math.floor((time - discriminant) / 2);
+        return positiveRoot - negativeRoot - 1;
     }
 
 }
