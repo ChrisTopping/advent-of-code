387c0475dcd2a9aac88a3cd12b55938551321f86
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-09 05:01:51 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day9;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 9, part = 2,
+        description = "Mirage Maintenance",
+        link = "https://adventofcode.com/2023/day/9#part2",
+        tags = ""
+)
+public class Day9Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part1Solution2023Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day9;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 9 Part 1")
+class Day9Part1Solution2023Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day9Part1Solution2023 solution = new Day9Part1Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
