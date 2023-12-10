125c5fd1cfdc22c2dc98bb9230e244f13a2766e8
ctopping <chris.p.topping@gmail.com>
2023-12-10 02:10:45 +0000

    Added 2023 day 9 parts 1 and 2

--- src/main/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part1Solution2023.java
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part1Solution2023.java
@@ -1,8 +1,12 @@
 package com.cpt.adventofcode.solution.year2023.day9;
 
 import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.helpers.SlidingWindow;
 import com.cpt.adventofcode.solution.Solution;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @AdventOfCodeSolution(year = 2023, day = 9, part = 1,
@@ -10,11 +14,28 @@ import java.util.stream.Stream;
         link = "https://adventofcode.com/2023/day/9",
         tags = ""
 )
-public class Day9Part1Solution2023 implements Solution<Integer> {
+public class Day9Part1Solution2023 implements Solution<Long> {
 
     @Override
-    public Integer solve(Stream<String> inputStream) {
-        return 0;
+    public Long solve(Stream<String> inputStream) {
+        return inputStream.map(s -> Arrays.stream(s.split("\\s+")).map(Long::parseLong).collect(Collectors.toList()))
+                .mapToLong(this::extrapolate)
+                .sum();
     }
+
+    private long extrapolate(List<Long> sequence) {
+        if (sequence.stream().allMatch(value -> value == 0)) {
+            return 0;
+        }
+
+        return extrapolate(differences(sequence)) + sequence.get(sequence.size() - 1);
+    }
+
+    private List<Long> differences(List<Long> sequence) {
+        return SlidingWindow.of(sequence, 2)
+                .map(longs -> longs.get(1) - longs.get(0))
+                .collect(Collectors.toList());
+    }
+
 
 }
--- src/test/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part2Solution2023Test.java
+++ src/test/java/com/cpt/adventofcode/solution/year2023/day9/Day9Part2Solution2023Test.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Day 9 Part 2")
-class Day9Part2Solution2023Test extends BaseTest<Integer> {
+class Day9Part2Solution2023Test extends BaseTest<Long> {
 
     @Test
-    @DisplayName("Given test input: should return 0")
+    @DisplayName("Given test input: should return 2")
     void givenTestInput_ShouldReturn0() {
         Day9Part2Solution2023 solution = new Day9Part2Solution2023();
-        assertSolutionIsCorrect(solution, 0);
+        assertSolutionIsCorrect(solution, 2L);
     }
 
 }
