03225a5ec3b8fd12d1bf474ac3afd733ef04ae80
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-10 05:01:47 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day10/Day10Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day10;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 10, part = 2,
+        description = "Pipe Maze",
+        link = "https://adventofcode.com/2023/day/10#part2",
+        tags = ""
+)
+public class Day10Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/cpt/adventofcode/solution/year2023/day10/Day10Part2Solution2023Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day10;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 10 Part 2")
+class Day10Part2Solution2023Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day10Part2Solution2023 solution = new Day10Part2Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
