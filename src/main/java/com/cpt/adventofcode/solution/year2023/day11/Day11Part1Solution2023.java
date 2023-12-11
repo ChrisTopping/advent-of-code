29e6ebb43fc5af1cb4017bd8ca3bd7238f4f512c
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-11 05:01:55 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day11/Day11Part1Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day11;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 11, part = 1,
+        description = "Cosmic Expansion",
+        link = "https://adventofcode.com/2023/day/11",
+        tags = ""
+)
+public class Day11Part1Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day11/Day11Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day11;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 11, part = 2,
+        description = "Cosmic Expansion",
+        link = "https://adventofcode.com/2023/day/11#part2",
+        tags = ""
+)
+public class Day11Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
