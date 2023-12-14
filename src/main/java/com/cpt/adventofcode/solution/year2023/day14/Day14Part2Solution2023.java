87f4f65ba90673278e0a3a4e003e67e569911cc0
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-14 05:01:39 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day14/Day14Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day14;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 14, part = 2,
+        description = "Parabolic Reflector Dish",
+        link = "https://adventofcode.com/2023/day/14#part2",
+        tags = ""
+)
+public class Day14Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
