c773d05f0c2d617ebd8f9f2da9592d48718ddff0
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-15 05:01:34 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day15/Day15Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day15;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 15, part = 2,
+        description = "Lens Library",
+        link = "https://adventofcode.com/2023/day/15#part2",
+        tags = ""
+)
+public class Day15Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/cpt/adventofcode/solution/year2023/day15/Day15Part1Solution2023Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day15;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 15 Part 1")
+class Day15Part1Solution2023Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day15Part1Solution2023 solution = new Day15Part1Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
