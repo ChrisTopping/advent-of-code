e0c27716287a37eb0b263c990321323a00b33dff
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-19 05:01:57 +0000

    Adding today's problem

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2023/day19/Day19Part1Solution2023IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2023.day19;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 19 Part 1")
+class Day19Part1Solution2023IntegrationTest extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day19Part1Solution2023 solution = new Day19Part1Solution2023();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
