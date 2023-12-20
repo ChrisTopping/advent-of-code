91fd83a88b992934687f9c2e986abd8f5833404c
ChrisTopping <ChrisTopping@users.noreply.github.com>
2023-12-20 05:01:33 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2023/day20/Day20Part2Solution2023.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2023.day20;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2023, day = 20, part = 2,
+        description = "Pulse Propagation",
+        link = "https://adventofcode.com/2023/day/20#part2",
+        tags = "unsolved"
+)
+public class Day20Part2Solution2023 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
