1cfa1d88feda84c0a01fdf93df5efd111eb7266d
ChrisTopping <ChrisTopping@users.noreply.github.com>
2024-12-01 05:01:46 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2024/day1/Day1Part1Solution2024.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2024.day1;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2024, day = 1, part = 1,
+        description = "Historian Hysteria",
+        link = "https://adventofcode.com/2024/day/1",
+        tags = "unsolved"
+)
+public class Day1Part1Solution2024 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
