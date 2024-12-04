707d14669f189e3484a36c60c7a599e10d73d202
chris.p.topping@gmail.com <ewmtfhgLaw.32>
2024-12-04 01:20:36 +0000

    Adding both solutions for 2024 day 2

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2024/day2/Day2Part1Solution2024IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2024.day2;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 2 Part 1")
+class Day2Part1Solution2024IntegrationTest extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 252")
+    void givenTestInput_ShouldReturn0() {
+        Day2Part1Solution2024 solution = new Day2Part1Solution2024();
+        assertSolutionIsCorrect(solution, 252);
+    }
+
+}
