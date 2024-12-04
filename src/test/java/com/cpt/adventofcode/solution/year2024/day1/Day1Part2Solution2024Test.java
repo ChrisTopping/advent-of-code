2eb636bb674132b039c78495c4463c63a324b1d4
ctopping <chris.p.topping@gmail.com>
2024-12-04 00:06:56 +0000

    Fixing tests for 2024 day 1

--- src/test/java/com/cpt/adventofcode/solution/year2024/day1/Day1Part2Solution2024Test.java
+++ src/test/java/com/cpt/adventofcode/solution/year2024/day1/Day1Part2Solution2024Test.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 class Day1Part2Solution2024Test extends BaseTest<Integer> {
 
     @Test
-    @DisplayName("Given test input: should return 0")
+    @DisplayName("Given test input: should return 31")
     void givenTestInput_ShouldReturn0() {
         Day1Part2Solution2024 solution = new Day1Part2Solution2024();
-        assertSolutionIsCorrect(solution, 0);
+        assertSolutionIsCorrect(solution, 31);
     }
 
 }
