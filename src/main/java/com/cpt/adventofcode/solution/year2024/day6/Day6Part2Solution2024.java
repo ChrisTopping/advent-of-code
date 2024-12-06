a5f6eba8da8d6e93bdc9786f7f10019be10eaccc
ChrisTopping <ChrisTopping@users.noreply.github.com>
2024-12-06 05:06:41 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2024/day6/Day6Part2Solution2024.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2024.day6;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2024, day = 6, part = 2,
+        description = "Guard Gallivant",
+        link = "https://adventofcode.com/2024/day/6#part2",
+        tags = "unsolved"
+)
+public class Day6Part2Solution2024 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/cpt/adventofcode/solution/year2024/day6/Day6Part1Solution2024Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2024.day6;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 6 Part 1")
+class Day6Part1Solution2024Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day6Part1Solution2024 solution = new Day6Part1Solution2024();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
