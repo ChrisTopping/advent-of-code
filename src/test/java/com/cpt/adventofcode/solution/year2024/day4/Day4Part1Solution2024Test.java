df71a65c69bec2f6543a8ad5374ad015ec837673
chris.p.topping@gmail.com <Chris Topping>
2024-12-08 16:38:59 +0000

    Added 2024 day 3 solutions

---
+++ src/test/java/com/cpt/adventofcode/solution/year2024/day4/Day4Part1Solution2024Test.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2024.day4;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 4 Part 1")
+class Day4Part1Solution2024Test extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 18")
+    void givenTestInput_ShouldReturn0() {
+        Day4Part1Solution2024 solution = new Day4Part1Solution2024();
+        assertSolutionIsCorrect(solution, 18);
+    }
+
+}
