5739d0c6e16eea756011e88c4dea660024fbd308
ChrisTopping <ChrisTopping@users.noreply.github.com>
2024-12-08 05:06:27 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2024/day8/Day8Part1Solution2024.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2024.day8;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2024, day = 8, part = 1,
+        description = "Resonant Collinearity",
+        link = "https://adventofcode.com/2024/day/8",
+        tags = "unsolved"
+)
+public class Day8Part1Solution2024 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
