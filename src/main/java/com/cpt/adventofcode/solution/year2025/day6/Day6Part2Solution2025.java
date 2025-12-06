bcc71ccdf7edac96b42162c86a79319c7bef46bb
Chris <chris.p.topping@gmail.com>
2025-12-06 11:28:27 +0000

    Consolidation sum in 2025 day 6 part 2

--- src/main/java/com/cpt/adventofcode/solution/year2025/day6/Day6Part2Solution2025.java
+++ src/main/java/com/cpt/adventofcode/solution/year2025/day6/Day6Part2Solution2025.java
@@ -3,7 +3,6 @@ package com.cpt.adventofcode.solution.year2025.day6;
 import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
 import com.cpt.adventofcode.solution.Solution;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 @AdventOfCodeSolution(year = 2025, day = 6, part = 2,
@@ -16,20 +15,19 @@ public class Day6Part2Solution2025 implements Solution<Long> {
     public Long solve(Stream<String> inputStream) {
         String[] input = inputStream.toArray(String[]::new);
 
-        String[] operations = input[input.length - 1].split("\\s+");
-        long[] answers = new long[operations.length];
+        long answer = 0;
 
         char operation = ' ';
-        int questionNumber = -1;
         long currentAnswer = 0;
+        
         for (int col = 0; col < input[0].length(); col++) {
             char operationAtCurrentCol = input[input.length - 1].charAt(col);
             if (operationAtCurrentCol != ' ') {
-                questionNumber++;
                 operation = operationAtCurrentCol;
-                answers[Math.max(questionNumber - 1, 0)] = currentAnswer;
+                answer += currentAnswer;
                 currentAnswer = operation == '*' ? 1 : 0;
             }
+            
             StringBuilder currentNumber = new StringBuilder();
             for (int row = 0; row < input.length - 1; row++) {
                 currentNumber.append(Character.toString(input[row].charAt(col)).strip());
@@ -42,9 +40,9 @@ public class Day6Part2Solution2025 implements Solution<Long> {
             }
         }
 
-        answers[questionNumber] = currentAnswer;
+        answer += currentAnswer;
 
-        return Arrays.stream(answers).sum();
+        return answer;
     }
 
 }
