4ba3922960f23cbdf0862cc0d2c6427a9c1e23b2
ChrisTopping <8250446+ChrisTopping@users.noreply.github.com>
2025-12-08 05:06:44 +0000

    Adding today's problem

---
+++ src/main/java/com/cpt/adventofcode/solution/year2025/day8/Day8Part2Solution2025.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2025.day8;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2025, day = 8, part = 2,
+        description = "Playground",
+        link = "https://adventofcode.com/2025/day/8#part2",
+        tags = "unsolved"
+)
+public class Day8Part2Solution2025 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
