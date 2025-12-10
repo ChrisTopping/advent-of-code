c17bfdd37b75bdc7996210172e66265bad7bd377
ChrisTopping <8250446+ChrisTopping@users.noreply.github.com>
2025-12-10 05:06:19 +0000

    Adding today's problem

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2025/day10/Day10Part2Solution2025IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2025.day10;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 10 Part 2")
+class Day10Part2Solution2025IntegrationTest extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day10Part2Solution2025 solution = new Day10Part2Solution2025();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
---
+++ src/main/java/com/cpt/adventofcode/solution/year2025/day10/Day10Part1Solution2025.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2025.day10;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2025, day = 10, part = 1,
+        description = "Factory",
+        link = "https://adventofcode.com/2025/day/10",
+        tags = "unsolved"
+)
+public class Day10Part1Solution2025 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
