740b15f001971d80ab9f973d6f9247bb22f57a11
ChrisTopping <8250446+ChrisTopping@users.noreply.github.com>
2025-12-12 05:05:55 +0000

    Adding today's problem

---
+++ src/integrationTest/java/com/cpt/adventofcode/solution/year2025/day12/Day12Part2Solution2025IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cpt.adventofcode.solution.year2025.day12;
+
+import com.cpt.adventofcode.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Day 12 Part 2")
+class Day12Part2Solution2025IntegrationTest extends BaseTest<Integer> {
+
+    @Test
+    @DisplayName("Given test input: should return 0")
+    void givenTestInput_ShouldReturn0() {
+        Day12Part2Solution2025 solution = new Day12Part2Solution2025();
+        assertSolutionIsCorrect(solution, 0);
+    }
+
+}
---
+++ src/main/java/com/cpt/adventofcode/solution/year2025/day12/Day12Part2Solution2025.java
@@ -0,0 +1,20 @@
+package com.cpt.adventofcode.solution.year2025.day12;
+
+import com.cpt.adventofcode.annotations.AdventOfCodeSolution;
+import com.cpt.adventofcode.solution.Solution;
+
+import java.util.stream.Stream;
+
+@AdventOfCodeSolution(year = 2025, day = 12, part = 2,
+        description = "Christmas Tree Farm",
+        link = "https://adventofcode.com/2025/day/12#part2",
+        tags = "unsolved"
+)
+public class Day12Part2Solution2025 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Stream<String> inputStream) {
+        return 0;
+    }
+
+}
